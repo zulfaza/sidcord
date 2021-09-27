2e1e7b900f1c61f2a481331890a88a17bfb63c17
zulfaza <32305083+zulfaza@users.noreply.github.com>
2021-09-28 00:32:53 +0700

    update logo

--- src/components/Navbar/index.js
+++ src/components/Navbar/index.js
@@ -6,6 +6,7 @@ import NavLink from "./NavLink";
 import MobileNavLink from "./MobileNavLink";
 import MobileUserNavLink from "./MobileUserNavLink";
 import ProfileDropDown from "./ProfileDropDown";
+import { Link } from "react-router-dom";
 
 const user = {
   name: "Tom Cook",
@@ -36,11 +37,13 @@ export default function Navbar() {
             <div className='flex items-center justify-between h-16'>
               <div className='flex items-center'>
                 <div className='flex-shrink-0'>
-                  <img
-                    className='h-8 w-8'
-                    src='https://tailwindui.com/img/logos/workflow-mark-indigo-500.svg'
-                    alt='Workflow'
-                  />
+                  <Link to='/'>
+                    <img
+                      className='h-8 w-8'
+                      src='https://tailwindui.com/img/logos/workflow-mark-indigo-500.svg'
+                      alt='Workflow'
+                    />
+                  </Link>
                 </div>
                 <div className='hidden md:block'>
                   <div className='ml-10 flex items-baseline space-x-4'>
