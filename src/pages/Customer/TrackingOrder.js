8fa4e4653755dbc6609db60e0d5782a9e605b3dd
zulfaza <32305083+zulfaza@users.noreply.github.com>
2021-11-03 12:12:08 +0700

    update

--- src/pages/Customer/TrackingOrder.js
+++ src/pages/Customer/TrackingOrder.js
@@ -90,7 +90,7 @@ export default function TrackingOrder() {
                           </td>
                           <td className='px-6 py-4 whitespace-nowrap'>
                             <span className='px-2 inline-flex text-xs leading-5 font-semibold rounded-full bg-green-100 text-green-800'>
-                              {getStatusLabel(product.status)}
+                              {getStatusLabel(order.status)}
                             </span>
                           </td>
                           <td className='px-6 py-4 whitespace-nowrap'>
